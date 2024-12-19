f1ecc15d8af0074acd93e8199b571a08b8699d9a
BalajiChandran <catchbaalaaji@gmail.com>
2024-12-19 16:17:18 +0530

    Added 12/19/2024 codes

--- src/test/java/com/balajichandran/ex02_RestAssuredBasics/Test006.java
+++ src/test/java/com/balajichandran/ex02_RestAssuredBasics/Test006.java
@@ -2,6 +2,6 @@ package com.balajichandran.ex02_RestAssuredBasics;
 
 public class Test006 {
     public static void main(String[] args) {
-        System.out.println("Hello 666");
+        System.out.println("Hello 66");
     }
 }
